8895a4a91909b0691deed5308ca4b65717b08a34
VTZamardeep <amardeepsingh.vtz@gmail.com>
2019-07-07 07:22:30 -1200

    section 9 variables and scope

---
+++ section-9_VariablesAndScope.js
@@ -0,0 +1,38 @@
+
+/**line 6 will give error because we are assigning const a value 
+ * also const  are immutables.
+*/
+let a=5;
+const b=5;
+a++;
+//b++;
+console.log(a+ 'and' +b);
+
+/**const array 
+ * we can change value in const array because as we are not changing the refrence of array 
+ * but when we change value of varibale its refrence gets changed.
+ */
+console.log('constant arrays');
+const arr=[5,7];
+arr.push(8);
+console.log(arr);//output will be 5,7,8
+
+/**
+ * arrays can be immutable by using freeze
+ */
+Object.freeze(arr);
+//now try to push values
+//uncomment next line error will be generated
+//arr.push(9);                            
+console.log(arr);
+
+
+/**
+ * const in loops 
+ * this question may arise how we can use const in loop?
+ * scope of const is block so we declare it fresh every time in below example.
+ */
+console.log('const in loops')
+for(const [index,value] of arr.entries()){
+    console.log(index+"....."+value)
+}
